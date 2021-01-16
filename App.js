7b59bace692a100e299f961798848ed550be6a92
zoraiz <hzoraiz8@gmail.com>
2021-01-16 19:04:59 +0500

    Added Ramdom play button, stop button, and redisgned most stuff

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Dimensions } from 'react-native';
-import DioButton from './components/DioButton'
-import { Audio } from 'expo-av'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ImageBackground, Image, Dimensions, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import { Audio } from 'expo-av';
+import DioButton from './components/DioButton';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height);
 
-let choice = 0;
-let firstRun = false;
+let titleWidth = Math.floor(screenWidth/8);
 
 const connections = [
   {id: 0, text: 'Kono Dio Da~!'},
@@ -36,106 +36,143 @@ const sounds = [
   {uri: require('./sounds/roller.mp3')},
 ];
 
-export default class App extends React.Component {
+let soundObjects = [];
 
-  pressed = async(text) => {
+let interval = setInterval(() => {}, 500);
+
+const App = () => {
+  const [ randomColor, setRandomColor ] = useState('red');
+  const [ randomText, setRandomText ] = useState('Play Randomly');
+
+  const pressed = async(text) => {
     var track = connections.filter(connection => connection.text === text);
-    choice = track[0].id;
+    let trackID = track[0].id;
 
-    // alert(screenWidth + ", " + screenHeight);
+    let soundObject = new Audio.Sound();
 
-    await this.componentDidMount();
+    try {
+
+      await soundObject.loadAsync(sounds[trackID].uri);
+      await playSound(soundObject);
+
+      soundObjects.push(soundObject);
+
+    } catch (e) {
+      stopAll();
+      alert('Sound component overloaded.' +
+      '\n\n(Note: If the keys don\'t work, please restart the app)');
+      console.log(e);
+    }
   }
 
-  async componentDidMount() {
+  const playSound = async(soundObject) => {
+    await soundObject.playAsync();
+  }
 
-    this.sound = new Audio.Sound();
-    try {
+  const random = () => {
+    stopAll()
+    if(randomText === 'Playing...')
+      return;
+
+    setRandomColor('green');
+    setRandomText('Playing...');
+
+    let song = connections[Math.floor(Math.random() * connections.length)].text;
+    pressed(song);
+    interval = setInterval(() => {
+      let song = connections[Math.floor(Math.random() * connections.length)].text;
+      pressed(song);
+    }, 1500);
+  }
+
+  const stopAll = async() => {
+    clearInterval(interval);
+    setRandomColor('red');
+    setRandomText('Play Randomly');
+    setTimeout(() => soundObjects.forEach(async(object) => await object.stopAsync()), 300)
+  }
+
+  useEffect(() => {
+    const firstLoad = async() => {
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: false,
         interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
         playsInSilentModeIOS: true,
         interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
         shouldDuckAndroid: true,
-        staysActiveInBackground: true,
+        staysActiveInBackground: false,
         playThroughEarpieceAndroid: false
       })
-
-      const status = {
-        shouldPlay: false
-      };
-
-      await this.sound.loadAsync(sounds[choice].uri, status, false);
-
-      if(firstRun)
-        await this.playSound();
-
-      firstRun = true;
-
-    } catch (e) {
-      alert('Sound component overloaded. Please slow down!!' +
-      '\n\n(Note: If the keys don\'t work, please restart this glorious app)');
     }
-  }
-
-  async playSound() {
-    await this.sound.playAsync();
-    // await this.sound.setPositionAsync(4);
-  }
+    firstLoad();
+  }, []);
   
-  render() {
-    let titleWidth = Math.floor(screenWidth/8);
-    let theManWidth = Math.floor(screenWidth/12);
-    let note = Math.floor(screenWidth/24);
-
-    return (
-      <View style={styles.container}>
+  return (
+    <View style={styles.container}>
       <ImageBackground source={require('./sources/It_Was_Me__Dio!.jpg')} style={styles.image} resizeMode='cover'>
-        <View style={styles.header}>  
-            <Text style={[styles.title, {fontSize: titleWidth}]}>Dio Voice Mesh</Text>
-            <Text style={[styles.theman, {fontSize: theManWidth}]}>By Zoraiz</Text>
-            <Text style={[styles.titlesmall, {fontSize: note}]}>(Note: Press buttons simultaneously)</Text>
-            <Text style={[styles.titlesmall, {fontSize: note}]}>v3.0</Text>
+        {/* <View style={styles.header}>  
+          <Text style={[styles.title, {fontSize: titleWidth}]}>Dio Voice Mesh</Text>
+        </View> */}
+        <Image
+          style={styles.titleImage}
+          source={require('./assets/title.png')}
+        />
+        <View style={styles.stopAndRandom}>
+          <TouchableOpacity delayPressIn={0} style={[styles.random, { flex: 1, marginRight: 3, backgroundColor: randomColor }]} onPress={random}>
+            <Text style={styles.randomText}>{ randomText }</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity delayPressIn={0} style={[styles.random, { flex: 1, marginLeft: 3, backgroundColor: 'black' }]} onPress={stopAll}>
+            <Text style={styles.randomText}>Stop</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.breaker}></View>
+        <View style={styles.buttons}>
+          <View style={styles.row}>
+            <DioButton text='Kono Dio Da~!' flexValue={1.3} pressed={pressed}/>
+            <DioButton text='Dio Scream' flexValue={1} pressed={pressed}/>
           </View>
-          <View style={styles.controls}>
-            <View style={styles.row}>
-              <DioButton text='Kono Dio Da~!' pressed={this.pressed}/>
-              <DioButton text='Dio Scream' pressed={this.pressed}/>
-            </View>
-            <View style={styles.row}>
-              <DioButton text='  Muda  ' pressed={this.pressed}/>
-              <DioButton text='Wryyyyy' pressed={this.pressed}/>
-            </View>
-            <View style={styles.row}>
-              <DioButton text='JoJo' pressed={this.pressed}/>
-              <DioButton text='ZA WARUDO~!' pressed={this.pressed}/>
-            </View>
-            <View style={styles.row}>
-              <DioButton text='Stando~Pawa' pressed={this.pressed}/>
-              <DioButton text='Counting' pressed={this.pressed}/>
-            </View>
-            <View style={styles.row}>
-              <DioButton text='The Approach' pressed={this.pressed}/>
-              <DioButton text='Road~Roller~Da' pressed={this.pressed}/>
-            </View>
+          <View style={styles.row}>
+            <DioButton text='  Muda  ' flexValue={1} pressed={pressed}/>
+            <DioButton text='Wryyyyy' flexValue={1} pressed={pressed}/>
           </View>
+          <View style={styles.row}>
+            <DioButton text='JoJo' flexValue={1} pressed={pressed}/>
+            <DioButton text='ZA WARUDO~!' flexValue={1.5} pressed={pressed}/>
+          </View>
+          <View style={styles.row}>
+            <DioButton text='Stando~Pawa' flexValue={1.2} pressed={pressed}/>
+            <DioButton text='Counting' flexValue={1} pressed={pressed}/>
+          </View>
+          <View style={styles.row}>
+            <DioButton text='The Approach' flexValue={1} pressed={pressed}/>
+            <DioButton text='Road~Roller~Da' flexValue={1.35} pressed={pressed}/>
+          </View>
+        </View>
       </ImageBackground>
-      
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+    
+      <StatusBar style='light' backgroundColor='black'/>
+    </View>
+  );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  titlesmall: {
-    color: 'white',
-    elevation: 3
+  container: {
+    flex: 1,
   },
 
-  theman: {
+  titlesmall: {
     color: 'white',
-    elevation: 3
+    elevation: 3,
+  },
+
+  titleImage: {
+    height: '8%',
+    width: '95%',
+    marginTop: 25,
+    resizeMode: 'stretch',
   },
 
   btt: {
@@ -144,48 +181,71 @@ const styles = StyleSheet.create({
   },
 
   image: {
+    marginTop: Constants.statusBarHeight,
     width: screenWidth,
-    height: screenHeight,
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
+    alignItems: 'center',
   },
 
   row: {
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+    width: '95%',
     padding: 3,
+    marginTop: 10,
+    // backgroundColor: 'red',
   },
 
   header: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    width: screenWidth,
-    // backgroundColor: 'blue',
-  },
-
-  controls: {
-    flex: 3,
-    width: 400,
-    justifyContent: 'center',
-    width: screenWidth,
-    // backgroundColor: 'red',
   },
 
   title: {
-    marginBottom: -5,
     color: 'white',
     fontWeight: 'bold',
     elevation: 3,
   },
 
-  container: {
-    flex: 1,
-    marginTop: 40,
+  buttons: {
+    flex: 6,
+    width: 400,
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    height: screenHeight,
     width: screenWidth,
+    // backgroundColor: 'red',
+  },
+  
+  stopAndRandom: {
+    width: '95%',
+    margin: 10,
+    flexDirection: 'row',
+  },
+
+  random: {
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 20,
+    elevation: 5,
+    opacity: 0.7,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  randomText: {
+    fontSize: 17,
+    color: 'white',
+    textAlign: 'center',
+  },
+
+  breaker: {
+    borderWidth: 0.3,
+    borderColor: 'white',
+    width: '95%',
   },
 });
--- components/DioButton.js
+++ components/DioButton.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions} from 'react-native';
 
-var size = 0;
-
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height);
 
-const setSize = (newSize) => {
-    size = newSize;
-}
-
-export default function DioButton( {textSize, text, pressed} ) {
-    let bttHeight = screenHeight/24;
+export default function DioButton( {flexValue, text, pressed} ) {
+    let bttHeight = screenHeight/18;
     let bttWidth = screenWidth/11;
 
     return (
-        <TouchableOpacity delayPressIn={0} onPress={async() => pressed(text)}>
+        <TouchableOpacity style={styles.btt, {flex: flexValue}} delayPressIn={0} onPress={() => pressed(text)}>
             <View style={[styles.btt, {paddingVertical: bttHeight, paddingHorizontal: bttWidth}]}>
                 <Text style={styles.text}>{ text }</Text>
             </View>
@@ -25,20 +19,22 @@ export default function DioButton( {textSize, text, pressed} ) {
 
 const styles = StyleSheet.create({
     btt: {
-        borderRadius: 25,
-        // paddingVertical: 30,
-        // paddingHorizontal: 40,
+        flex: 1,
+        borderRadius: 20,
         backgroundColor: 'blue',
         marginLeft: 5,
         marginRight: 5,
         elevation: 5,
         opacity: 0.7,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 
     text: {
-        color: 'white',
-        fontWeight: 'bold',
         fontSize: 15,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
         textAlign: 'center',
     }
 });
